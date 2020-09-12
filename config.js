a581ba6cd365dd321d9923ec7d62adfcd97c30c8
Shivansh Handa <avihs.29@gmail.com>
2020-09-12 19:38:46 +0530

    personlised starter-lumen

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Byte Blog by Shivansh',
+  subtitle: 'Byte, for your mega apetite of knowledge',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Shivansh Handa',
+    photo: '/photo1_crop.jpg',
+    bio: 'Trying to be a Software Engineer...(-^-)', 
+    contacts: {
+      email: 'avihs.29@gmail.com',
+      facebook: 'shivansh.handa/',
+      telegram: '',
+      twitter: '',
+      github: 'DarkByt31',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'shivansh-handa/',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
---
+++ content/pages/contacts.md
@@ -0,0 +1,8 @@
+---
+title: "Contact me"
+template: "page"
+socialImage: "/media/image-4.jpg"
+---
+
+Best place to contact: [avihs.29@gmail.com ](avihs.29@gmail.com) :)  
+Put `Blog:` in the title for faster reply.
